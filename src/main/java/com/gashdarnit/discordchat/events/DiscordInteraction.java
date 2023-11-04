b7418724d4373e17d520c767c891b39b8493676c
GashDarnit <edisontanh@gmail.com>
2023-11-05 03:04:08 +0800

    Added death message display

--- src/main/java/com/gashdarnit/discordchat/events/DiscordInteraction.java
+++ src/main/java/com/gashdarnit/discordchat/events/DiscordInteraction.java
@@ -9,18 +9,19 @@ public class DiscordInteraction implements Listener {
     private static String fileDir = "Replace this part with the desired directory" + "\\data\\";
 
     @EventHandler
+    private void playerDeathMessageListener(PlayerDeathEvent event) {
+        String addMessage = event.getDeathMessage();
+
+        addMessageToFile("[Server]: " + addMessage);
+    }
+
+    @EventHandler
     private void advancementMessageListener(PlayerAdvancementDoneEvent event) {
         String playerName = event.getPlayer().getName();
         String advancement = event.getAdvancement().getDisplay().getTitle();
         String addMessage = "[Server]: " + playerName + " has made the advancement [" + advancement + "]";
 
-        try {
-            FileWriter fw = new FileWriter(fileDir + "minecraft_chat.txt", true);
-            fw.write(addMessage);
-            fw.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        addMessageToFile(addMessage);
     }
 
     @EventHandler
@@ -29,6 +30,10 @@ public class DiscordInteraction implements Listener {
         String message = event.getMessage();
         String addMessage = "[" +  playerName + "]: " + message;
 
+        addMessageToFile(addMessage);
+    }
+
+    private void addMessageToFile(String addMessage) {
         try {
             FileWriter fw = new FileWriter(fileDir + "minecraft_chat.txt", true);
             fw.write(addMessage);
